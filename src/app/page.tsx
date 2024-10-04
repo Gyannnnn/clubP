ff1fd56a559c3a66d887fa1f615921e3466ff5c4
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-10-04 10:26:25 +0530

    Added Files

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Left from '@/Pages/Components/Left'
+import EventsAndNews from '@/Pages/EventsAndNews'
+import Temp from '@/Pages/Components/temp'
+
+export default function page() {
+  return (
+    <div>
+      <EventsAndNews/>
+      {/* <Temp/> */}
+      
+     
+    </div>
+  )
+}
